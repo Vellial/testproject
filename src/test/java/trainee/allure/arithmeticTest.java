c1537c10d6566ac2068396a2edc17ea9be83f8c1
Svetlana_S <lost.life.omsk@gmail.com>
2017-08-31 16:31:06 +0600

    Parametrized tests

--- src/test/java/trainee/allure/arithmeticTest.java
+++ src/test/java/trainee/allure/arithmeticTest.java
@@ -1,33 +1,43 @@
 package trainee.allure;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import ru.yandex.qatools.allure.annotations.*;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assume.assumeThat;
 
 /**
  * Created by Vellial on 28.08.2017.
  */
+@RunWith(Parameterized.class)
 @Title("Arithmetic test suite. +, -, *, /")
 public class arithmeticTest {
+
+    public int op1;
+    public int op2;
+    public String operation;
+    public int result;
+
+    public arithmeticTest(int op1, int op2, String operation, int result) {
+        this.op1 = op1;
+        this.op2 = op2;
+        this.operation = operation;
+        this.result = result;
+    }
 
     @Title("Check addition operation")
     @Description("In this test we will check addition operation")
     @Test
     public void additionTest() {
-        String additionOperators = getStrFromFile(0);
-        String[] params = additionOperators.split(";");
-        Integer op1 = Integer.valueOf(params[0]);
-        Integer op2 = Integer.valueOf(params[1]);
-        String operation = params[2];
-        Integer result = Integer.valueOf(params[3]);
+        assumeThat(operation, is("+"));
         checkResult(getAdditionResult(op1, op2, operation), result);
     }
 
@@ -35,12 +45,7 @@ public class arithmeticTest {
     @Description("In this test we will check substraction operation")
     @Test
     public void substractionTest() {
-        String substractionOperators = getStrFromFile(1);
-        String[] params = substractionOperators.split(";");
-        Integer op1 = Integer.valueOf(params[0]);
-        Integer op2 = Integer.valueOf(params[1]);
-        String operation = params[2];
-        Integer result = Integer.valueOf(params[3]);
+        assumeThat(operation, is("-"));
         checkResult(getSubstractionResult(op1, op2, operation), result);
     }
 
@@ -48,12 +53,7 @@ public class arithmeticTest {
     @Description("In this test we will check division operation")
     @Test
     public void divisionTest() {
-        String divisionOperators = getStrFromFile(3);
-        String[] params = divisionOperators.split(";");
-        Integer op1 = Integer.valueOf(params[0]);
-        Integer op2 = Integer.valueOf(params[1]);
-        String operation = params[2];
-        Integer result = Integer.valueOf(params[3]);
+        assumeThat(operation, is("/"));
         checkResult(getDivisionResult(op1, op2, operation), result);
     }
 
@@ -61,12 +61,7 @@ public class arithmeticTest {
     @Description("In this test we will check multiplication operation")
     @Test
     public void multiplicationTest() {
-        String multiplicationOperators = getStrFromFile(2);
-        String[] params = multiplicationOperators.split(";");
-        Integer op1 = Integer.valueOf(params[0]);
-        Integer op2 = Integer.valueOf(params[1]);
-        String operation = params[2];
-        Integer result = Integer.valueOf(params[3]);
+        assumeThat(operation, is("*"));
         checkResult(getMultiplicationResult(op1, op2, operation), result);
     }
 
@@ -100,6 +95,35 @@ public class arithmeticTest {
     private int getMultiplicationResult(int firstInt, int secondInt, String operation) {
         return firstInt * secondInt;
     }
+
+    @Parameterized.Parameters
+    public static List<String[]> getTestData() {
+        String[][] data = new String[4][4];
+        try {
+            ClassLoader classLoader = arithmeticTest.class.getClassLoader();
+            File file = new File(classLoader.getResource("data").getFile());
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            List<String> lines = new ArrayList<String>();
+            int i = 0;
+            while ((line = reader.readLine()) != null) {
+                String[] params = line.split(";");
+                for (int j = 0; j < params.length; j++) {
+//                    System.out.println(Arrays.toString(params));
+                    String op1 = params[j];
+                    data[i][j] = op1;
+                }
+                lines.add(line);
+                i++;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Arrays.asList(data);
+    }
+
 
     // Get input data from file for tests.
     private String getStrFromFile(int numberOfLine) {
